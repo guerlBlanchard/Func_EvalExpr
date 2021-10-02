37cc20d44c307243f5b91ee589b003e2bef002a2
Jamal KANSAO <jamal.kansao@epitech.eu>
2021-10-02 17:51:19 +0200

    [MOD] switching type to data and transcribe the parsing function accordingly

--- src/Parse.hs
+++ src/Parse.hs
@@ -21,72 +21,84 @@ module Parse
 
 import Text.Read
 
-type Parser a = String -> Maybe (a, String)
-
-parseTuple :: Parser a -> Parser (a,a)
-parseTuple a x = case parseChar '(' x of
-    Just (_, par1) -> case a par1 of
-        Just (r1, x1) -> case parseChar ',' x1 of
-            Just (_, vir) -> case a vir of
-                Just (r2, par2) -> case parseChar ')' x of
-                    Just (_, rest) -> Just ((r1, r2), rest)
-                    Nothing -> Nothing
-                Nothing -> Nothing
-            Nothing -> Nothing
-        Nothing -> Nothing
-    Nothing -> Nothing
-
-parseInt :: Parser Int
-parseInt x = case parseChar '-' x of
-    Just (_, xr1) -> case parseUInt xr1 of
-        Just (r, xr2) -> Just (-r, xr2)
-        Nothing -> Nothing
-    Nothing -> parseUInt x
-
-parseUInt :: Parser Int
-parseUInt x = case parseSome (parseAnyChar ['0'..'9']) x of
-    Just (r, xr) -> Just (read r :: Int, xr)
-    Nothing -> Nothing
-
-parseSome :: Parser a -> Parser [a]
-parseSome a (x:xs) = case a (x:xs) of
-    Just (r, xr) -> Just ([r] ++ jr, jx)
-        where Just (jr, jx) = parseMany a xs
-    Nothing -> Nothing
-
-parseMany :: Parser a -> Parser [a]
-parseMany a (x:xs) = case a (x:xs) of
-    Just (r, xr) -> Just ([r] ++ jr, jx)
-        where Just (jr, jx) = parseMany a xs
-    Nothing -> Just ([], (x:xs))
-
-parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
-parseAndWith function a b x = case a x of
-    Just (r1, xs) -> case b xs of
-        Just (r2, xr) -> Just (function r1 r2, xr)
-        Nothing -> Nothing
-    Nothing -> Nothing
-
-parseAnd :: Parser a -> Parser b -> Parser (a, b)
-parseAnd a b x = case a x of
-    Just (r1, xs) -> case b xs of
-        Just (r2, xr) -> Just ((r1, r2), xr)
-        Nothing -> Nothing
-    Nothing -> Nothing
-
-parseOr :: Parser a -> Parser a -> Parser a
-parseOr a b x = case a x of
-    Just (r, xr) -> Just (r, xr)
-    Nothing -> b x
-
-parseAnyChar :: String -> Parser Char
-parseAnyChar _ "" = Nothing
-parseAnyChar "" b = Nothing
-parseAnyChar a (b:bs)
-        | elem b a = Just(b, bs)
-        | otherwise = Nothing 
+data Parser a = Parser {
+    runParser :: String -> Maybe (a, String)
+}
 
 parseChar :: Char -> Parser Char
-parseChar _ [] = Nothing
-parseChar a (x:xs)  | a == x = Just (x, xs)
+parseChar a = Parser func where
+              func [] = Nothing
+              func (x:xs)
+                    | a == x = Just (x, xs)
                     | otherwise = Nothing
+
+parseAnyChar :: String -> Parser Char
+parseAnyChar a = Parser func where
+                    func [] = Nothing
+                    func (b:bs)
+                        | b `elem` a = Just(b, bs)
+                        | otherwise = Nothing
+
+parseOr :: Parser a -> Parser a -> Parser a
+parseOr (Parser b) (Parser c) = Parser func where
+                func x = case b x of
+                    Just (r, xr) -> Just (r, xr)
+                    Nothing -> c x
+
+parseAnd :: Parser a -> Parser b -> Parser (a, b)
+parseAnd (Parser a) (Parser b) = Parser func where
+                func x = case a x of
+                    Just (r1, xs) -> case b xs of
+                        Just (r2, xr) -> Just ((r1, r2), xr)
+                        Nothing -> Nothing
+                    Nothing -> Nothing
+
+parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+parseAndWith function (Parser a) (Parser b) = Parser func where 
+                func x = case a x of
+                    Just (r1, xs) -> case b xs of
+                        Just (r2, xr) -> Just (function r1 r2, xr)
+                        Nothing -> Nothing
+                    Nothing -> Nothing
+
+parseMany :: Parser a -> Parser [a]
+parseMany (Parser a) = Parser func where
+                    func (x:xs) = case a (x:xs) of
+                        Just (r, xr) -> Just ([r] ++ jr, jx)
+                            where Just (jr, jx) = runParser (parseMany (Parser a)) xs
+                        Nothing -> Just ([], (x:xs))
+
+parseSome :: Parser a -> Parser [a]
+parseSome (Parser a) = Parser func where
+                    func (x:xs) = case a (x:xs) of
+                        Just (r, xr) -> Just ([r] ++ jr, jx)
+                            where Just (jr, jx) = runParser (parseMany (Parser a)) xs
+                        Nothing -> Nothing
+
+parseUInt :: Parser Int
+parseUInt = Parser func where
+                    func x = case runParser (parseSome (parseAnyChar ['0'..'9'])) x of
+                        Just (r, xr) -> Just (read r :: Int, xr)
+                        Nothing -> Nothing
+
+parseInt :: Parser Int
+parseInt = Parser func where
+                    func x = case runParser (parseChar '-') x of
+                        Just (_, xr1) -> case runParser parseUInt xr1 of
+                            Just (r, xr2) -> Just (-r, xr2)
+                            Nothing -> Nothing
+                        Nothing -> runParser parseUInt x
+
+parseTuple :: Parser a -> Parser (a,a)
+parseTuple (Parser a) = Parser func where
+                    func x = case runParser (parseChar '(') x of
+                        Just (_, par1) -> case a par1 of
+                            Just (r1, x1) -> case runParser (parseChar ',') x1 of
+                                Just (_, vir) -> case a vir of
+                                    Just (r2, par2) -> case runParser (parseChar ')') x of
+                                        Just (_, rest) -> Just ((r1, r2), rest)
+                                        Nothing -> Nothing
+                                    Nothing -> Nothing
+                                Nothing -> Nothing
+                            Nothing -> Nothing
+                        Nothing -> Nothing
