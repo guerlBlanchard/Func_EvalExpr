d3f69ecc6d016caa7e38cb74ffe843de6cf3f428
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2021-10-11 18:03:34 +0200

    [ADD] improved the grammar [ADD] start of the combinators

--- src/EvalExpr.hs
+++ src/EvalExpr.hs
@@ -11,14 +11,29 @@ module EvalExpr
 
 import Parse
 
-eval:: String -> Int 
+data EXPR = ADD EXPR EXPR
+        | SUB EXPR EXPR
+        | MUL EXPR EXPR
+        | DIV EXPR EXPR
+        | POW EXPR EXPR
+        | DIG Int 
+
+expr :: EXPR -> Int 
+expr x = case x of
+    ADD i j -> expr i + expr j
+    SUB i j -> expr i - expr j
+    MUL i j -> expr i * expr j
+    DIV i j -> expr i / expr j
+    POW i j -> expr i ^ expr j
+    DIG i -> i
+
+eval :: Parser EXPR 
 eval = add
     where
-        add = parseOr (mul) (parseOr (mul + add) (mul - add))
-        mul = parseOr (pow) (parseOr (pow * pow) (pow / pow))
-        pow = parseOr (num) (num ^ pow)
-        num = parseOr (dig) (add)
-        dig = parseInt 
+        add = parseOr mul (parseOr (mul + add) (mul - add))
+        mul = parseOr pow (parseOr (pow * pow) (pow / pow))
+        pow = parseOr num (num ^ pow)
+        num = parseOr (DIG) (add)
 
-evalExpr :: String -> Maybe Int
-evalExpr = eval
+evalExpr :: String -> Int
+evalExpr = fmap expr $ runParser eval
