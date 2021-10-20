c24c32c47bb9be45ae8cad29940dae565e477637
Jamal KANSAO <jamal.kansao@epitech.eu>
2021-10-20 18:48:03 +0200

    [MOD] optimizing Makefile for less compilation time + adding more unit tests

--- Makefile
+++ Makefile
@@ -9,7 +9,19 @@ STACK_PATH := $(shell stack path --local-install-root)
 
 NAME = funEvalExpr
 
-all:
+MAIN    := app/Main.hs
+
+SRC        := src/EvalExpr.hs
+SRC        += src/Parse.hs
+
+TESTSRC := test/Spec.hs
+
+OBJ := $(MAIN:.hs=.o) $(MAIN:.hs=.hi)
+OBJ += $(SRC:.hs=.o) $(SRC:.hs=.hi)
+OBJ += $(TESTSRC:.hs=.o) $(TESTSRC:.hs=.hi)
+
+
+all: $(SRC) $(MAIN)
 	stack build
 	cp $(STACK_PATH)/bin/funEvalExpr-exe ./$(NAME)
 
@@ -24,4 +36,4 @@ fclean:	clean
 
 re:	fclean all
 
-.PHONY: all clean fclean re
+.PHONY: all tests_run clean fclean re
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,7 @@ main :: IO Counts
 main = runTestTT myTest
 
 myTest :: Test
-myTest = test [assertEqual "Basic Test of wrong parserChar" (Just ('b',"bc")) (runParser (parseChar 'a') "abc"),
-                assertEqual "Basic Test of right parserChar" (Just ('a',"bc")) (runParser (parseChar 'a') "abc"),
+myTest = test [ assertEqual "Basic Test of right parserChar" (Just ('a',"bc")) (runParser (parseChar 'a') "abc"),
                 assertEqual "Basic Test of parserChar returning Nothing" (Nothing) (runParser (parseChar 'z') "abcd"),
-                assertEqual "Basic Test of right parserAnyChar" (Just ('a',"bcd")) (runParser (parseAnyChar "bca") "abcd")]
+                assertEqual "Basic Test of right parserAnyChar" (Just ('a',"bcd")) (runParser (parseAnyChar "bca") "abcd"),
+                assertEqual "Basic Test of parserAnyChar returning Nothing" (Nothing) (runParser (parseAnyChar "xyz") "abcd")]
