84130604dde0f582f5cd1ef23fc864e5ec6a873b
Guerlain BLANCHARD <guerlain.blanchard@epitech.eu>
2021-10-21 09:39:26 +0200

    [ADD] number of arguments error handling

--- app/Main.hs
+++ app/Main.hs
@@ -7,5 +7,10 @@ import EvalExpr
 main :: IO ()
 main = do
     args <- getArgs
+    errorHandling $ length args
     evalExpr (head args)
     exitWith (ExitSuccess)
+
+errorHandling :: Int  -> IO ()
+errorHandling 1 = return()
+errorHandling _ = exitWith(ExitFailure 84)
